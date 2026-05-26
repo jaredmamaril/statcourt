446d777d9bfaf0179b8ef2fc32670023029dad2d
mattjaredmamaril <tytjared@gmail.com>
2026-05-26 00:04:53 -0500

    Create headings for page purpose

--- app/court/page.tsx
+++ app/court/page.tsx
@@ -2,11 +2,15 @@
   return (
     <main className="min-h-screen bg-[#07111f] text-white">
       <section className="relative flex min-h-screen items-center justify-between bg-[url('/court.svg')] bg-cover bg-center bg-no-repeat px-6 sm:px-10">
-        <div className="pointer-events-none absolute left-[39%] top-10 -translate-x-1/2 px-4 font-michroma text-base text-[#4FB3D8] drop-shadow-[0_0_6px_rgba(0,0,0,1)]">
-          STATCOURT
+        <div className="absolute left-0 top-0 z-10 h-full w-1/2">
+          <h1 className="absolute left-10 top-8 font-michroma text-2xl px-10 py-6 text-white font-bold">
+            CHOOSE YOUR PLAYER
+          </h1>
         </div>
-        <div className="pointer-events-none absolute bottom-10 left-[61%] -translate-x-1/2 px-4 font-michroma text-base text-[#4FB3D8] drop-shadow-[0_0_6px_rgba(0,0,0,1)]">
-          STATCOURT
+        <div className="absolute right-0 top-0 z-10 h-full w-1/2">
+          <h1 className="absolute right-10 top-8 font-michroma text-2xl px-10 py-6  text-white font-bold">
+            CHOOSE YOUR PLAYER
+          </h1>
         </div>
       </section>
     </main>
