9f1f7a5dc3e0df9b25ea4673a97df2eef8fe340c
mattjaredmamaril <tytjared@gmail.com>
2026-05-26 13:56:58 -0500

    Create TypeScript types for stats and players' info for further safety of error detection

--- app/court/page.tsx
+++ app/court/page.tsx
@@ -4,26 +4,74 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Court() {
-  const players = [
+  type PlayerStats = {
+    ppg: number; // Points Per Game
+    rpg: number; // Rebounds Per Game
+    apg: number; // Assists Per Game
+    fgPercent: number; // Field Goal Percentage
+    threePercent: number; // Three Point Percentage
+    ftPercent: number; // Free Throw Percentage
+  };
+
+  type Player = {
+    value: string;
+    label: string;
+    image: string;
+    stats: PlayerStats;
+  };
+
+  const players: Player[] = [
     {
       value: "lebron-james",
       label: "LeBron James",
       image: "/temp-players/lebron-james.png",
+      stats: {
+        ppg: 27.0,
+        rpg: 7.4,
+        apg: 8.3,
+        fgPercent: 50.4,
+        threePercent: 34.5,
+        ftPercent: 73.4,
+      },
     },
     {
       value: "michael-jordan",
       label: "Michael Jordan",
       image: "/temp-players/michael-jordan.jpg",
+      stats: {
+        ppg: 30.1,
+        rpg: 6.2,
+        apg: 3.4,
+        fgPercent: 49.7,
+        threePercent: 37.9,
+        ftPercent: 83.5,
+      },
     },
     {
       value: "kobe-bryant",
       label: "Kobe Bryant",
       image: "/temp-players/kobe-bryant.jpg",
+      stats: {
+        ppg: 25.0,
+        rpg: 5.2,
+        apg: 4.7,
+        fgPercent: 44.7,
+        threePercent: 32.9,
+        ftPercent: 83.7,
+      },
     },
     {
       value: "stephen-curry",
       label: "Stephen Curry",
       image: "/temp-players/stephen-curry.png",
+      stats: {
+        ppg: 24.2,
+        rpg: 4.6,
+        apg: 6.5,
+        fgPercent: 47.7,
+        threePercent: 43.3,
+        ftPercent: 90.6,
+      },
     },
   ];
 
