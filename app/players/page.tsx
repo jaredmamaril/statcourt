8f2d5111ad33ef99cd69371dac643d1265cf0bce
mattjaredmamaril <tytjared@gmail.com>
2026-05-30 16:00:00 -0500

    Add player selection component basis with list of players and picking players

---
+++ app/players/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { players } from "../components/court-data";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Players() {
+  const [currentPlayer, setCurrentPlayer] = useState("");
+  const [playerSearch, setPlayerSearch] = useState("");
+  const filteredPlayers = players.filter((player) =>
+    player.name.toLowerCase().includes(playerSearch.toLowerCase()),
+  );
+
+  return (
+    <main className="min-h-screen overflow-hidden text-white overflow-y-auto">
+      <section className="min-h-screen container mx-auto px-6 pt-10 pb-12 flex flex-col items-center relative">
+        {/* Wrapper that slides everything */}
+        <div
+          className={`flex flex-col items-center transition-transform duration-3000 ease-out ${
+            currentPlayer ? "translate-x-[-180%]" : "translate-x-0"
+          }`}
+        >
+          {/* Header section */}
+          <div className="flex flex-col items-center justify-between gap-3 mb-6">
+            <h1 className="font-michroma text-lg font-bold tracking-wide text-[#1bc2ec]">
+              CHOOSE A PLAYER
+            </h1>
+
+            <input
+              value={playerSearch}
+              onChange={(e) => setPlayerSearch(e.target.value)}
+              placeholder="Search for a Player..."
+              className="w-full sm:w-64 rounded-md border border-white/30 bg-black/40 px-4 py-2 text-white/80 placeholder:text-[#2da6c4]/80 font-michroma text-sm backdrop-blur-sm outline-none focus:border-white text-center"
+            />
+          </div>
+
+          {/* Player list */}
+          <div className="flex flex-col w-full max-w-100 gap-1 max-h-[70vh] overflow-y-auto pr-2">
+            {filteredPlayers.map((player) => {
+              const isSelected = player.name === currentPlayer;
+              return (
+                <button
+                  key={player.id}
+                  type="button"
+                  onClick={() =>
+                    setCurrentPlayer(
+                      currentPlayer === player.name ? "" : player.name,
+                    )
+                  }
+                  className={`cursor-pointer flex items-center w-full px-4 py-4 text-left font-michroma text-xs rounded-md border transition-all duration-200
+                    ${
+                      isSelected
+                        ? "border-[#178aa7] bg-[#1bc2ec]/10 text-[#1bc2ec]"
+                        : "border-white/10 bg-black/20 text-white/90 hover:bg-white/5 hover:border-white/30"
+                    }`}
+                >
+                  {player.name}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
