859a72800803cc4acdca9b463410414aad590eac
mattjaredmamaril <tytjared@gmail.com>
2026-06-21 17:55:00 -0500

    Add image configuration for NBA headshots in Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.nba.com",
+        pathname: "/headshots/nba/latest/1040x760/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
